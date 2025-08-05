ddb50b66a52db64785b45520cbf65ffc4c86ebba
Yashraj Anand Chavan <yashraj19012007@gmail.com>
2025-08-05 10:50:24 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -13,7 +13,6 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
 app.use(express.json());
 
 const allowedOrigins = [
@@ -144,4 +143,5 @@ app.post('/execute-sql', async (req, res) => {
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
 });
+
 
